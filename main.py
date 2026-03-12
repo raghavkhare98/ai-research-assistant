6a3f8ce99ffa8e77106f85c97fbe707ab7b9511f
raghavkhare98 <khare08rma@gmail.com>
2026-03-12 16:25:24 -0400

    Create web_search tool

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import openai
 import json
 import os
+import datetime
+import urllib.request
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -8,3 +10,16 @@ load_dotenv()
 OPENAI_KEY = os.environ.get('OPENAI_KEY')
 
 client = openai.OpenAI(api_key=OPENAI_KEY)
+
+def web_search(query: str) -> str:
+    if ["_", ".", "-", "~"] not in query:
+        encoded_url = urllib.parse.quote(query)
+        url = f"https://api.duckduckgo.com/?q={encoded_url}&format=json&no_html=1"
+    try:
+        with urllib.request.urlopen(url, timeout=5) as r:
+            data = json.loads(r.read())
+            abstract = data.get("AbstractText", "")
+            related = [t["Text"] for t in data.get("RelatedTopics", [])[:3] if "Text" in t]
+            return abstract or "\n".join(related) or "No results found"
+    except:
+        return "Search Unavailable"
